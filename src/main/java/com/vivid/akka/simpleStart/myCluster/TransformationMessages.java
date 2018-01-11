e27d9bbd85fe8694e8e27166695260b0fa4825a8
vivid <luoxin@vcmy.com>
2018-01-11 14:44:51 +0800

    cluster

---
+++ src/main/java/com/vivid/akka/simpleStart/myCluster/TransformationMessages.java
@@ -0,0 +1,81 @@
+package com.vivid.akka.simpleStart.myCluster;
+
+import java.io.Serializable;
+
+/** 
+* 
+* @author luoxin
+* @Time 创建时间：2018年1月11日 下午2:13:11 
+* @version 
+*/
+
+
+public class TransformationMessages {
+
+	/**
+	 * 传递的数据（参数）
+	 */
+	public static class TransformationJob implements Serializable {
+		private final String text;
+
+		public TransformationJob(String text) {
+			this.text = text;
+		}
+
+		public String getText() {
+			return text;
+		}
+	}
+
+	/**
+	 * 返回结果
+	 */
+	public static class TransformationResult implements Serializable {
+		private final String text;
+
+		public TransformationResult(String text) {
+			this.text = text;
+		}
+
+		public String getText() {
+			return text;
+		}
+
+		@Override
+		public String toString() {
+			return "TransformationResult(" + text + ")";
+		}
+	}
+
+	/**
+	 * 异常处理
+	 */
+	public static class JobFailed implements Serializable {
+		private final String reason;
+		private final TransformationJob job;
+
+		public JobFailed(String reason, TransformationJob job) {
+			this.reason = reason;
+			this.job = job;
+		}
+
+		public String getReason() {
+			return reason;
+		}
+
+		public TransformationJob getJob() {
+			return job;
+		}
+
+		@Override
+		public String toString() {
+			return "JobFailed(" + reason + ")";
+		}
+	}
+
+	/**
+	 * 用于服务端向客户端注册
+	 */
+	public static final int BACKEND_REGISTRATION = 1;
+
+}
